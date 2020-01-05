5716d2fdcbe6ccde8777d57d1fee733b49b12c38
DashyAmarillo <58460010+dasher107@users.noreply.github.com>
2020-01-05 14:19:23 +0700

    Add files via upload

---
+++ mb.py
@@ -0,0 +1,12 @@
+import os,sys
+import random
+wer = "Djdjfhfhrhz-$+$+4++ch'f6'x4'yg7bu:7bi!l:K:8u;jv6&Yv8-f7&O-i-oi-igihi"
+cc = 374757372919384831010101010292938348475773838484858584848447447473733828282919292928383833939393838383939399393393993393939393339393948858585885858585858548848
+while 1:
+    print wer
+    print "\033[1;32m attachEvent(onloa,c);}).call(this);(function(){var b=[];google.jsc={xx:b,x:function(a){b.push(a)},mm:[],m:.ion(a,d){return function(c){c||(c=window.event);return d.call(a,c)}},E=!=typeof navigator&&/Macintosh/.test(navigator.userAgent),F=function()\033[0m"
+    ##
+    print cc
+    cc = cc + 9999999999999999999374748283949382829394657393029484864464467337373373739229292874593758365938638538538538538535396406460406963582742472852583953593963959
+    print "\033[33m %s"%(cc)
+    print "\033[31m jsc.mm=a)}};}).call(this);(function(){var g=this||self,h=Date.now||function(){returnMac"
